1515a7af1b67543e299b59835bab0052b01540f4
Dimitris Papagiannis <nothingface0@gmail.com>
2022-07-25 14:19:13 +0200

    Found the manual!

--- odp_test.py
+++ odp_test.py
@@ -1,19 +1,53 @@
 from odf.opendocument import OpenDocumentText, OpenDocumentPresentation
-from odf.style import Style, TextProperties
+from odf.style import (
+    Style,
+    MasterPage,
+    PageLayout,
+    PageLayoutProperties,
+    TextProperties,
+    GraphicProperties,
+    ParagraphProperties,
+    DrawingPageProperties,
+)
+from odf.text import P
 from odf.presentation import Header
 from odf.text import Title, H
+from odf.draw import Page, Frame, TextBox, Image
 
 
 class Presentation(object):
     def __init__(self):
         self.doc = OpenDocumentPresentation()
 
+        pagelayout = PageLayout(name="MyLayout")
+        self.doc.automaticstyles.addElement(pagelayout)
+        pagelayout.addElement(
+            PageLayoutProperties(
+                margin="0cm",
+                pagewidth="28cm",
+                pageheight="21cm",
+                printorientation="landscape",
+            )
+        )
+
+        titlestyle = Style(name="MyMaster-title", family="presentation")
+        titlestyle.addElement(ParagraphProperties(textalign="center"))
+        titlestyle.addElement(TextProperties(fontsize="34pt"))
+        titlestyle.addElement(GraphicProperties(fillcolor="#ffff99"))
+        self.doc.styles.addElement(titlestyle)
+
+        photostyle = Style(name="MyMaster-photo", family="presentation")
+        self.doc.styles.addElement(photostyle)
+
+        masterpage = MasterPage(name="MyMaster", pagelayoutname=pagelayout)
+        self.doc.masterstyles.addElement(masterpage)
+
     def save(self, filename: str) -> None:
         self.doc.save(outputfile=filename)
 
     def add_header(self, header_text: str):
         # odf.element.IllegalChild: <text:title> is not allowed in <office:presentation>
-        # h = Title(text=header_text)
+        h = Title(text=header_text)
 
         # odf.element.IllegalChild: <presentation:header> is not allowed in <office:presentation>
         # h = Header()
@@ -27,7 +61,7 @@ class Presentation(object):
 
 def main():
     p = Presentation()
-    p.add_header("OMG LOL")
+    # p.add_header("OMG LOL")
     p.save(filename="test.odp")
 
 
